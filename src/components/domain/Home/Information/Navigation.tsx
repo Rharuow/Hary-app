71db4d1e0bd295474c2117310228e701c93ab05d
Harysson <haryssonsoares@gmail.com>
2022-12-28 22:24:25 -0300

    feat: about me page

--- src/components/domain/Home/Information/Navigation.tsx
+++ src/components/domain/Home/Information/Navigation.tsx
@@ -3,16 +3,16 @@ import React from "react";
 
 const Navigation: React.FC<{ className?: string }> = ({ className = " " }) => {
   return (
-    <div className="d-flex justify-content-around">
+    <div className={`d-flex justify-content-around ${className}`}>
       <Link href="/" className="text-gray-500 text-yellow-hover">
         Home
       </Link>
 
-      <Link href="/" className="text-gray-500 text-yellow-hover">
+      <Link href="/about-me" className="text-gray-500 text-yellow-hover">
         Sobre mim
       </Link>
 
-      <Link href="/" className="text-gray-500 text-yellow-hover">
+      <Link href="/my-goals" className="text-gray-500 text-yellow-hover">
         Meus objetivos
       </Link>
     </div>
--- src/components/domain/Home/Information/index.tsx
+++ src/components/domain/Home/Information/index.tsx
@@ -7,7 +7,7 @@ const Information = () => {
   return (
     <div className="d-flex flex-column-reverse pb-0 pe-0 w-50">
       <Avatar width={357} height={500} className="align-self-end" />
-      <Navigation className="align-self-center" />
+      <Navigation className="align-self-center w-100" />
     </div>
   );
 };
